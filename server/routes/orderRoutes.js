69a01b7590d34599dab613b26e0e8d304d931247
rohitraj70 <rohit.tech0956@gmail.com>
2026-08-30 19:18:50 +0530

    Fix order API registration and backend startup

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createOrder, getOrders, updateOrderStatus } from "../controllers/orderController.js";
+import { createOrder, getOrderById, getOrders, updateOrderStatus } from "../controllers/orderController.js";
 import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
