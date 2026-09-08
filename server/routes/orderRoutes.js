6a39e48487fb2197fc9ceae114df425303582ba1
rohitraj70 <rohit.tech0956@gmail.com>
2026-09-08 21:00:14 +0530

    Allow multiple customer orders

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,11 +1,10 @@
 import express from "express";
 import { createOrder, getOrderById, getOrders, updateOrderStatus } from "../controllers/orderController.js";
 import { protect } from "../middleware/auth.js";
-import { orderRateLimit } from "../middleware/orderRateLimit.js";
 
 const router = express.Router();
 
-router.post("/", orderRateLimit, createOrder);
+router.post("/", createOrder);
 router.get("/", protect, getOrders);
 router.get("/:id", getOrderById);
 router.put("/:id/status", protect, updateOrderStatus);
